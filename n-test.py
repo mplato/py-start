52acd3ae222c7ddfe9acba2e3e44cfd9ffba36e5
mplato <731650130@seznam.cz>
2019-08-12 10:04:01 +0200

    numpy

--- n-test.py
+++ n-test.py
@@ -2,7 +2,11 @@ import numpy as np
 
 
 def sigmoid(x):
-    return 1/(1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derivatives(x):
+    return x * (1 - x)
 
 
 trainingInput = np.array([[0, 0, 1],
@@ -10,8 +14,24 @@ trainingInput = np.array([[0, 0, 1],
                           [1, 0, 1],
                           [0, 1, 1]])
 
-trainingOutput = np.array([[0, 1, 1, 1]]).T
+trainingOutput = np.array([[0, 1, 1, 0]]).T
 
 np.random.seed(1)
 
 synapitcWheights = 2 * np.random.random((3, 1)) - 1
+
+print('Random starting synaptic weights: ')
+print(synapitcWheights)
+
+for i in range(5):
+    inputLayer = trainingInput
+    output = sigmoid(np.dot(inputLayer, synapitcWheights))
+    error = trainingOutput - output
+    adjustment = error * sigmoid_derivatives(output)
+    synapitcWheights += np.dot(inputLayer.T, adjustment)
+
+print('\nSynaptic weights after training: ')
+print(synapitcWheights)
+
+print('\nOutput after training: ')
+print(output)
